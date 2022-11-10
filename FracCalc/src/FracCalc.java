acaf69d073fd2227ab762fbca3fb6d9eafd0a883
Cailean Fernandes <551096@my.cuhsd.org>
2022-11-10 09:59:18 -0800

    Going through checkpoint 2

--- FracCalc/src/FracCalc.java
+++ FracCalc/src/FracCalc.java
@@ -5,18 +5,18 @@ public class FracCalc {
     {
         // TODO: Read the input from the user and call produceAnswer with an equation
         Scanner reader = new Scanner(System.in);
-        String input = reader.nextLine();
-        System.out.println(produceAnswer(input));
+        boolean inputting = true;
+        while (inputting) {
+            String input = reader.nextLine();
+            System.out.println(produceAnswer(input));
+            if (input == "quit") {
+                inputting = false;
+            }
+        }
+
+
     }
 
-    // ** IMPORTANT ** DO NOT DELETE THIS FUNCTION.  This function will be used to test your code
-    // This function takes a String 'input' and produces the result
-    //
-    // input is a fraction string that needs to be evaluated.  For your program, this will be the user input.
-    //      e.g. input ==> "1/2 + 3/4"
-    //
-    // The function should return the result of the fraction after it has been calculated
-    //      e.g. return ==> "1_1/4"
     public static String produceAnswer(String input)
     {
         // TODO: Implement this function to produce the solution to the input
@@ -24,9 +24,16 @@ public class FracCalc {
         String firstOper = arr[0];
         String operator = arr[1];
         String secondOper = arr[2];
-        return secondOper;
+
+        String[] secondOperWholeArr = secondOper.split("_");
+        String secondOperWhole = secondOperWholeArr[0];
+        String secondOperFraction = secondOperWholeArr[1];
+
+        String[] secondOperNumArr = secondOperFraction.split("/");
+        String secondOperNum = secondOperNumArr[0];
+
+        String secondOperDen = secondOperNumArr[1];
+
+        return ("whole:" + secondOperWhole + " numerator:" + secondOperNum + " denominator:" + secondOperDen);
     }
-
-    // TODO: Fill in the space below with any helper methods that you think you will need
-
 }
